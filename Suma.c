87adb78473f616a5ed7588be2b70ac553a28da3b
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-11-29 15:53:21 -0500

    \Suma.c Inclusion de librerias y creación de la estructura main

---
+++ Suma.c
@@ -0,0 +1,10 @@
+/*Realice un programa donde muestre dos matrices generadas aleatoriamente entre 0 y 100, para finalmente mostrar la suma de ambas*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
