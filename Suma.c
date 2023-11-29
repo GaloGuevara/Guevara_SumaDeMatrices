2212ce2a81b6a5e2a52354299dc31bf91a1bd208
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-11-29 15:56:27 -0500

    \Suma.c Bloque de codigo donde se rellena las matrices con numeros aleatorios entre 0 y 100, y se muestra en pantalla las mismas

--- Suma.c
+++ Suma.c
@@ -10,5 +10,44 @@ int main(int argc, char const *argv[])
     scanf("%d%*c", &n);
     int m1[n][n];
     int m2[n][n];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            m1[i][j] = rand() % 101;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            m2[i][j] = rand() % 101;
+        }
+    }
+
+    puts("\nLa matriz 1 es:");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d\t", m1[i][j]);
+        }
+        printf("\n");
+    }
+
+    puts("\nLa matriz 2 es: ");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d\t", m2[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
